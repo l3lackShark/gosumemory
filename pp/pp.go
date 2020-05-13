5fbe220b653f8e1e5150a18c8851d672bfc23b7a
blackshark <lea2616@gmail.com>
2020-05-14 03:33:24 +0500

    Revert some pp stuff

--- pp/pp.go
+++ pp/pp.go
@@ -1,11 +1,12 @@
 package pp
 
 import (
-	"log"
+	"errors"
 	"strings"
 	"time"
 	"unsafe"
 
+	"github.com/k0kubun/pp"
 	"github.com/l3lackShark/gosumemory/memory"
 	"github.com/spf13/cast"
 )
@@ -14,62 +15,131 @@ import (
 //#cgo CPPFLAGS: -DOPPAI_STATIC_HEADER
 //#include <stdlib.h>
 //#include "oppai.c"
+// import "C"
+
+//#cgo LDFLAGS: -lm
+//#cgo CPPFLAGS: -DOPPAI_STATIC_HEADER
+//#include <stdlib.h>
+//#include "oppai.c"
 import "C"
 
-//GetData resolves pp values (using cgo)
+var ez C.ezpp_t
+
+type PP struct {
+	Total         C.float
+	FC            C.float
+	StarRating    C.float
+	AimStars      C.float
+	SpeedStars    C.float
+	AimPP         C.float
+	SpeedPP       C.float
+	Accuracy      C.float
+	N300          C.int
+	N100          C.int
+	N50           C.int
+	NMiss         C.int
+	AR            C.float
+	CS            C.float
+	OD            C.float
+	HP            C.float
+	Artist        string
+	ArtistUnicode string
+	Title         string
+	TitleUnicode  string
+	Version       string
+	Creator       string
+	NCircles      C.int
+	NSliders      C.int
+	NSpinners     C.int
+	ODMS          C.float
+	Mode          C.int
+	Combo         C.int
+	MaxCombo      C.int
+	Mods          C.int
+	ScoreVersion  C.int
+}
+
+func readData(data *PP, ez C.ezpp_t) error {
+	path := (memory.SongsFolderPath + "/" + memory.MenuData.Bm.Path.BeatmapFolderString + "/" + memory.MenuData.Bm.Path.BeatmapOsuFileString) //TODO: Automatic Songs folder finder
+
+	if strings.HasSuffix(path, ".osu") && memory.DynamicAddresses.IsReady == true {
+		cpath := C.CString(path)
+
+		defer C.free(unsafe.Pointer(cpath))
+		if rc := C.ezpp(ez, cpath); rc < 0 {
+			return errors.New(C.GoString(C.errstr(rc)))
+		}
+		C.ezpp_set_base_ar(ez, C.float(memory.MenuData.Bm.Stats.BeatmapAR))
+		C.ezpp_set_base_od(ez, C.float(memory.MenuData.Bm.Stats.BeatmapOD))
+		C.ezpp_set_base_cs(ez, C.float(memory.MenuData.Bm.Stats.BeatmapCS))
+		C.ezpp_set_base_hp(ez, C.float(memory.MenuData.Bm.Stats.BeatmapHP))
+	}
+	C.ezpp_set_accuracy_percent(ez, C.float(memory.GameplayData.Accuracy))
+	C.ezpp_set_mods(ez, C.int(memory.GameplayData.Mods.AppliedMods))
+
+	//C.ezpp_set_score_version(ez)
+	C.ezpp_set_end_time(ez, C.float(memory.MenuData.Bm.Time.PlayTime))
+	C.ezpp_set_combo(ez, C.int(memory.GameplayData.Combo.Max))
+	C.ezpp_set_nmiss(ez, C.int(memory.GameplayData.Hits.H0))
+	*data = PP{
+		Total:         C.ezpp_pp(ez),
+		StarRating:    C.ezpp_stars(ez),
+		AimStars:      C.ezpp_aim_stars(ez),
+		SpeedStars:    C.ezpp_speed_stars(ez),
+		AimPP:         C.ezpp_aim_pp(ez),
+		SpeedPP:       C.ezpp_speed_pp(ez),
+		Accuracy:      C.ezpp_accuracy_percent(ez),
+		N300:          C.ezpp_n300(ez),
+		N100:          C.ezpp_n100(ez),
+		N50:           C.ezpp_n50(ez),
+		NMiss:         C.ezpp_nmiss(ez),
+		AR:            C.ezpp_ar(ez),
+		CS:            C.ezpp_cs(ez),
+		OD:            C.ezpp_od(ez),
+		HP:            C.ezpp_hp(ez),
+		Artist:        C.GoString(C.ezpp_artist(ez)),
+		ArtistUnicode: C.GoString(C.ezpp_artist_unicode(ez)),
+		Title:         C.GoString(C.ezpp_title(ez)),
+		TitleUnicode:  C.GoString(C.ezpp_title_unicode(ez)),
+		Version:       C.GoString(C.ezpp_version(ez)),
+		Creator:       C.GoString(C.ezpp_creator(ez)),
+		NCircles:      C.ezpp_ncircles(ez),
+		NSliders:      C.ezpp_nsliders(ez),
+		NSpinners:     C.ezpp_nspinners(ez),
+		ODMS:          C.ezpp_odms(ez),
+		Mode:          C.ezpp_mode(ez),
+		Combo:         C.ezpp_combo(ez),
+		MaxCombo:      C.ezpp_max_combo(ez),
+		Mods:          C.ezpp_mods(ez),
+		ScoreVersion:  C.ezpp_score_version(ez),
+	}
+	return nil
+}
+
 func GetData() {
+	ez := C.ezpp_new()
+	defer C.ezpp_free(ez)
+	C.ezpp_set_autocalc(ez, 1)
+
 	for {
-		ez := C.ezpp_new()
-		C.ezpp_set_autocalc(ez, 1)
-		path := (memory.SongsFolderPath + "/" + memory.MenuData.Bm.Path.BeatmapFolderString + "/" + memory.MenuData.Bm.Path.BeatmapOsuFileString) //TODO: Automatic Songs folder finder
-		var tempPath string
-		if strings.HasSuffix(path, ".osu") && path != tempPath && memory.DynamicAddresses.IsReady == true {
-			tempPath = path
-			cpath := C.CString(path)
-			defer C.free(unsafe.Pointer(cpath))
-			if rc := C.ezpp(ez, cpath); rc < 0 {
-				log.Println((C.GoString(C.errstr(rc))))
-			}
-			switch memory.MenuData.OsuStatus {
-			case 2:
-				//C.ezpp_set_accuracy_percent(ez, C.float(math.Round(memory.GameplayData.Accuracy*100)/100))
-				C.ezpp_set_accuracy_percent(ez, C.float(memory.GameplayData.Accuracy))
-				//C.ezpp_set_accuracy(ez, C.int(int(memory.GameplayData.Hits.H100)), C.int(int(memory.GameplayData.Hits.H50)))
-				C.ezpp_set_mods(ez, C.int(memory.GameplayData.Mods.AppliedMods))
-				if memory.GameplayData.Combo.Max > 0 {
-					memory.GameplayData.PP.PPifFC = cast.ToInt32(float64(C.ezpp_pp(ez)))
-					C.ezpp_set_combo(ez, C.int(memory.GameplayData.Combo.Max))
-					C.ezpp_set_nmiss(ez, C.int(memory.GameplayData.Hits.H0))
-					C.ezpp_set_end_time(ez, C.float(memory.MenuData.Bm.Time.PlayTime))
-					memory.GameplayData.PP.Pp = cast.ToInt32(float64(C.ezpp_pp(ez)))
-				}
 
-			default:
-				C.ezpp_set_base_ar(ez, C.float(memory.MenuData.Bm.Stats.BeatmapAR))
-				C.ezpp_set_base_od(ez, C.float(memory.MenuData.Bm.Stats.BeatmapOD))
-				C.ezpp_set_base_cs(ez, C.float(memory.MenuData.Bm.Stats.BeatmapCS))
-				C.ezpp_set_base_hp(ez, C.float(memory.MenuData.Bm.Stats.BeatmapHP))
-				memory.MenuData.Bm.Metadata.Artist = C.GoString(C.ezpp_artist(ez))
-				memory.MenuData.Bm.Metadata.Title = C.GoString(C.ezpp_title(ez))
-				memory.MenuData.Bm.Metadata.Version = C.GoString(C.ezpp_version(ez))
-				memory.MenuData.Bm.Metadata.Mapper = C.GoString(C.ezpp_creator(ez))
-				memory.MenuData.PP.PpSS = cast.ToInt32(float64(C.ezpp_pp(ez)))
-				C.ezpp_set_accuracy_percent(ez, C.float(99.0))
-				memory.MenuData.PP.Pp99 = cast.ToInt32(float64(C.ezpp_pp(ez)))
-				C.ezpp_set_accuracy_percent(ez, C.float(98.0))
-				memory.MenuData.PP.Pp98 = cast.ToInt32(float64(C.ezpp_pp(ez)))
-				C.ezpp_set_accuracy_percent(ez, C.float(97.0))
-				memory.MenuData.PP.Pp97 = cast.ToInt32(float64(C.ezpp_pp(ez)))
-				C.ezpp_set_accuracy_percent(ez, C.float(96.0))
-				memory.MenuData.PP.Pp96 = cast.ToInt32(float64(C.ezpp_pp(ez)))
-				C.ezpp_set_accuracy_percent(ez, C.float(95.0))
-				memory.MenuData.PP.Pp95 = cast.ToInt32(float64(C.ezpp_pp(ez)))
-
-			}
+		var data PP
+		err := readData(&data, ez)
+		if err != nil {
+			pp.Println(err)
+		}
+		//	pp.Println(data.Total)
+		if cast.ToInt32(float64(data.Total)) >= 0 && memory.MenuData.OsuStatus == 2 {
+			memory.GameplayData.PP.Pp = cast.ToInt32(float64(data.Total))
+		}
+		if memory.MenuData.OsuStatus == 5 {
+			memory.GameplayData.Hits.H100 = 0
+			memory.GameplayData.Hits.H50 = 0
+			memory.GameplayData.Hits.H0 = 0
+			memory.GameplayData.PP.Pp = 0
 
 		}
-		C.ezpp_free(ez)
+
 		time.Sleep(time.Duration(memory.UpdateTime) * time.Millisecond)
 	}
-
 }
