8dae98b8f1c6f163d8dc46c7821b4469cd78fe56
Tadeo Kondrak <me@tadeo.ca>
2020-07-13 20:13:37 -0600

    mem: Read memory incrementally
    Try to use a smart search using the reasonably optimized
    bytes.IndexByte for most of the heavy lifting.
    
    Should drastically reduce memory usage.

--- mem/mem.go
+++ mem/mem.go
@@ -1,6 +1,7 @@
 package mem
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"io"
@@ -66,20 +67,78 @@ func parsePattern(s string) (pattern, error) {
 	return p, nil
 }
 
-func Scan(p Process, pattern string) (uint64, error) {
+func maskbyte(needle, mask uint32) (search byte, offset int) {
+	for offset := 0; offset < 4; offset++ {
+		maskByte := byte(mask >> (offset * 8) & 0xFF)
+		needleByte := byte(needle >> (offset * 8) & 0xFF)
+		if maskByte != 0x00 && needleByte != 0x00 {
+			return needleByte, offset
+		}
+	}
+	panic("empty mask (bad pattern)")
+}
+
+func search(buf []byte, pat pattern) (int, bool) {
+	progress := 0
+	maskbyte, byteoffset := maskbyte(pat.Bytes[0], pat.Mask[0])
+	for {
+		i := bytes.IndexByte(buf, maskbyte)
+		if i == -1 {
+			return 0, false
+		}
+		begin := i - byteoffset
+		end := begin + len(pat.Bytes)*4
+		if begin < 0 || end > len(buf) {
+			progress += i + 1
+			buf = buf[i+1:]
+			continue
+		}
+		success := true
+		for j := range pat.Bytes {
+			needle, mask := pat.Bytes[j], pat.Mask[j]
+			slice := buf[begin+(j*4) : begin+(j*4)+4]
+			haystack := binary.LittleEndian.Uint32(slice)
+			if needle^haystack&mask != 0 {
+				success = false
+				break
+			}
+		}
+		if success {
+			return begin + progress, true
+		}
+		progress += i + 1
+		buf = buf[i+1:]
+	}
+}
+
+func find(p Process, pat pattern, reg Map) (int64, error) {
+	const bufsize = 65536
+	var buf [bufsize]byte
+	for i := int64(0); i < reg.Size(); {
+		ntoread := reg.Size() - i
+		if ntoread >= bufsize {
+			ntoread = bufsize - 1
+		}
+		n, err := p.ReadAt(buf[:ntoread], int64(reg.Start()+i))
+		if err != nil {
+			return 0, err
+		}
+		if at, ok := search(buf[:n], pat); ok {
+			return int64(reg.Start() + i + int64(at)), nil
+		}
+		diff := n - len(pat.Bytes)*8
+		if diff <= 1 {
+			diff = 1
+		}
+		i += int64(diff)
+	}
+	return 0, ErrPatternNotFound
+}
+
+func Scan(p Process, pattern string) (int64, error) {
 	maps, err := p.Maps()
 	if err != nil {
 		return 0, err
-	}
-
-	var largestMap uint64
-	for _, reg := range maps {
-		if size := reg.Size(); size > largestMap && size < 1073741824 {
-			largestMap = size
-		}
-	}
-	if largestMap == 0 {
-		return 0, ErrPatternNotFound
 	}
 
 	pat, err := parsePattern(pattern)
@@ -87,38 +146,12 @@ func Scan(p Process, pattern string) (uint64, error) {
 		return 0, err
 	}
 
-	buf := make([]byte, int(largestMap))
 	for _, reg := range maps {
-		addr := reg.Start()
-		size := reg.Size()
-		if size >= 1073741824 {
-			continue
-		}
-		n, err := p.ReadAt(buf[0:size], int64(addr))
-		if err != nil || n != int(size) {
-			continue
-		}
-		needle := pat.Bytes[0]
-		mask := pat.Mask[0]
-
-		var i uint64
-	outer:
-		for i = 0; i+4 < size; i++ {
-			haystack := binary.LittleEndian.Uint32(buf[i : i+4])
-			if needle^haystack&mask == 0 {
-				for j := range pat.Bytes {
-					needle := pat.Bytes[j]
-					mask := pat.Mask[j]
-					haystack := binary.LittleEndian.Uint32(
-						buf[i+uint64(4*j) : i+4+uint64(4*j)])
-					if needle^haystack&mask != 0 {
-						continue outer
-					}
-				}
-				return reg.Start() + i, nil
-			}
+		if i, err := find(p, pat, reg); err == nil {
+			return i, nil
 		}
 	}
+
 	return 0, ErrPatternNotFound
 }
 
