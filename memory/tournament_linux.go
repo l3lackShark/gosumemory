e60f6759e5264f489812743e81bb9fb2280d2b48
l3lackShark <lea2616@gmail.com>
2020-10-20 17:33:35 +0500

    Tournament: Add missing package to Linux

--- memory/tournament_linux.go
+++ memory/tournament_linux.go
@@ -1,5 +1,12 @@
 //+build linux
 
+package memory
+
+import (
+	"errors"
+
+	"github.com/l3lackShark/gosumemory/mem"
+)
 
 func resolveTourneyClients(procs []mem.Process) ([]mem.Process, error) {
 	return nil, errors.New("Not implemented!")
