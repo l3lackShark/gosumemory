163922d7909a52864a431215731bd5f534c3cff4
Mikhail Babynichev <i@kotrik.ru>
2023-03-17 19:21:22 +0300

    fix: new offsets b20230313

--- memory/read.go
+++ memory/read.go
@@ -75,7 +75,7 @@ func (staticAddresses) PlayContainer() string {
 }
 
 func (staticAddresses) Leaderboard() string {
-	return "[[[LeaderboardBase+0x1] + 0x4] + 0x74] + 0x24"
+	return "[[[LeaderboardBase+0x1] + 0x4] + 0x7C] + 0x24"
 }
 
 type menuD struct {
@@ -128,24 +128,24 @@ type allTimesD struct {
 }
 type gameplayD struct {
 	Retries             int32   `mem:"[Base - 0x33] + 0x8"`
-	PlayerName          string  `mem:"[[[Ruleset + 0x60] + 0x38] + 0x28]"`
-	ModsXor1            int32   `mem:"[[[Ruleset + 0x60] + 0x38] + 0x1C] + 0xC"`
-	ModsXor2            int32   `mem:"[[[Ruleset + 0x60] + 0x38] + 0x1C] + 0x8"`
-	HitErrors           []int32 `mem:"[[[Ruleset + 0x60] + 0x38] + 0x38]"`
-	Mode                int32   `mem:"[[Ruleset + 0x60] + 0x38] + 0x64"`
-	MaxCombo            int16   `mem:"[[Ruleset + 0x60] + 0x38] + 0x68"`
-	Score               int32   `mem:"[[Ruleset + 0x60] + 0x38] + 0x78"`
+	PlayerName          string  `mem:"[[[Ruleset + 0x68] + 0x38] + 0x28]"`
+	ModsXor1            int32   `mem:"[[[Ruleset + 0x68] + 0x38] + 0x1C] + 0xC"`
+	ModsXor2            int32   `mem:"[[[Ruleset + 0x68] + 0x38] + 0x1C] + 0x8"`
+	HitErrors           []int32 `mem:"[[[Ruleset + 0x68] + 0x38] + 0x38]"`
+	Mode                int32   `mem:"[[Ruleset + 0x68] + 0x38] + 0x64"`
+	MaxCombo            int16   `mem:"[[Ruleset + 0x68] + 0x38] + 0x68"`
+	Score               int32   `mem:"[[Ruleset + 0x68] + 0x38] + 0x78"`
 	ScoreV2             int32   `mem:"Ruleset + 0xF8"`
-	Hit100              int16   `mem:"[[Ruleset + 0x60] + 0x38] + 0x88"`
-	Hit300              int16   `mem:"[[Ruleset + 0x60] + 0x38] + 0x8A"`
-	Hit50               int16   `mem:"[[Ruleset + 0x60] + 0x38] + 0x8C"`
-	HitGeki             int16   `mem:"[[Ruleset + 0x60] + 0x38] + 0x8E"`
-	HitKatu             int16   `mem:"[[Ruleset + 0x60] + 0x38] + 0x90"`
-	HitMiss             int16   `mem:"[[Ruleset + 0x60] + 0x38] + 0x92"`
-	Combo               int16   `mem:"[[Ruleset + 0x60] + 0x38] + 0x94"`
-	PlayerHPSmooth      float64 `mem:"[[Ruleset + 0x60] + 0x40] + 0x14"`
-	PlayerHP            float64 `mem:"[[Ruleset + 0x60] + 0x40] + 0x1C"`
-	Accuracy            float64 `mem:"[[Ruleset + 0x60] + 0x48] + 0xC"`
-	LeaderBoard         uint32  `mem:"[Ruleset + 0x74] + 0x24"`
-	KeyOverlayArrayAddr uint32  `mem:"[[Ruleset + 0xA8] + 0x10] + 0x4"` //has to be at the end due to mem not liking dead pointers, TODO: Fix this mem-side
+	Hit100              int16   `mem:"[[Ruleset + 0x68] + 0x38] + 0x88"`
+	Hit300              int16   `mem:"[[Ruleset + 0x68] + 0x38] + 0x8A"`
+	Hit50               int16   `mem:"[[Ruleset + 0x68] + 0x38] + 0x8C"`
+	HitGeki             int16   `mem:"[[Ruleset + 0x68] + 0x38] + 0x8E"`
+	HitKatu             int16   `mem:"[[Ruleset + 0x68] + 0x38] + 0x90"`
+	HitMiss             int16   `mem:"[[Ruleset + 0x68] + 0x38] + 0x92"`
+	Combo               int16   `mem:"[[Ruleset + 0x68] + 0x38] + 0x94"`
+	PlayerHPSmooth      float64 `mem:"[[Ruleset + 0x68] + 0x40] + 0x14"`
+	PlayerHP            float64 `mem:"[[Ruleset + 0x68] + 0x40] + 0x1C"`
+	Accuracy            float64 `mem:"[[Ruleset + 0x68] + 0x48] + 0xC"`
+	LeaderBoard         uint32  `mem:"[Ruleset + 0x7C] + 0x24"`
+	KeyOverlayArrayAddr uint32  `mem:"[[Ruleset + 0xB0] + 0x10] + 0x4"` //has to be at the end due to mem not liking dead pointers, TODO: Fix this mem-side
 }
